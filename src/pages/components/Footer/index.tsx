7cc5cb9e97c764b1211ffc51fef6746219919978
naive <33138729+xinkeji@users.noreply.github.com>
2023-12-23 19:02:38 +0800

    Update index.tsx

--- src/pages/components/Footer/index.tsx
+++ src/pages/components/Footer/index.tsx
@@ -7,7 +7,28 @@ export default function Footer() {
   return (
     <footer className="flex flex-col items-center pb-4">
       <Image src="/icon/line.svg" width="125" height="54" />
-     
+      <div className="flex justify-center mt-10">
+        <a
+          className="transition hover:underline"
+          href="https://github.com/mayandev/notion-avatar"
+        >
+          {t(`github`)}
+        </a>
+        <span className="mx-2">·</span>
+        <a
+          className="transition hidden md:inline-block hover:underline"
+          href="https://dribbble.com/phillzou"
+        >
+          {t(`dribbble`)}
+        </a>
+        <span className="mx-2 hidden md:inline-block">·</span>
+        <a className="hover:underline" href="https://twitter.com/phillzou">
+          {t(`twitter`)}
+        </a>
+        <span className="mx-2">·</span>
+        <a className="transition hover:underline" href={t(`coffeeUrl`)}>
+          {t(`coffee`)}
+        </a>
       </div>
       <div className="text-gray-500 mt-3 px-6 text-center">
         <a
